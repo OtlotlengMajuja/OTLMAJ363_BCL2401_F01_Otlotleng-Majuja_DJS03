ad1be68ad92c8d8d024cad7b9bd4dc72bc996634
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-05-15 15:00:37 +0200

    Included the 'populateGenres' method to iterate over genres and create element options and set the default to all genres

--- StudentNo_Classcode_Group_Name-Surname_DJS03-main/scripts.js
+++ StudentNo_Classcode_Group_Name-Surname_DJS03-main/scripts.js
@@ -46,23 +46,23 @@ renderBooks(books) {
     document.querySelector('[data-list-items]').appendChild(fragment)
 }
 
+populateGenres() {
+    const genreHtml = document.createDocumentFragment();
+    const firstGenreElement = document.createElement('option');
+    firstGenreElement.value = 'any';
+    firstGenreElement.innerText = 'All Genres';
+    genreHtml.appendChild(firstGenreElement);
 
+    for (const [id, name] of Object.entries(genres)) {
+        const element = document.createElement('option');
+        element.value = id;
+        element.innerText = name;
+        genreHtml.appendChild(element);
+    }
 
-
-const genreHtml = document.createDocumentFragment()
-const firstGenreElement = document.createElement('option')
-firstGenreElement.value = 'any'
-firstGenreElement.innerText = 'All Genres'
-genreHtml.appendChild(firstGenreElement)
-
-for (const [id, name] of Object.entries(genres)) {
-    const element = document.createElement('option')
-    element.value = id
-    element.innerText = name
-    genreHtml.appendChild(element)
+    document.querySelector('[data-search-genres]').appendChild(genreHtml)
 }
 
-document.querySelector('[data-search-genres]').appendChild(genreHtml)
 
 const authorsHtml = document.createDocumentFragment()
 const firstAuthorElement = document.createElement('option')
