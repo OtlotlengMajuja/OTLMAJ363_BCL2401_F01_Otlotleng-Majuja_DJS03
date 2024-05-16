ac3f644b0b7939cbbb61ac4ab7d64f8f8ca79aa1
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-05-16 21:03:03 +0200

    Included a function that sets the book's cover image, title, and author in the corresponding elements of the overlay

--- StudentNo_Classcode_Group_Name-Surname_DJS03-main/scripts.js
+++ StudentNo_Classcode_Group_Name-Surname_DJS03-main/scripts.js
@@ -197,7 +197,14 @@ handleBookClick(Event) {
     }
 }
 
-
+openBookDetail(book) {
+    document.querySelector('[data-list-active]').open = true;
+    document.querySelector('[data-list-blur]').src = book.image;
+    document.querySelector('[data-list-image]').src = book.image;
+    document.querySelector('[data-list-title]').innerText = book.title;
+    document.querySelector('[data-list-subtitle]').innerText = `${this.authors[book.author]} (${new Date(book.published).getFullYear()})`;
+    document.querySelector('[data-list-description]').innerText = book.description;
+}
 
 
 
